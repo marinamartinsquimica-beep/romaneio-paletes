5b2b9b0fcef0f35077a7a4d226fb52ced19d54f9
marinamartinsquimica-beep <marinamartinsquimica@gmail.com>
2026-06-20 19:56:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = "romaneio-cache-v9";
+const CACHE_NAME = "romaneio-cache-v10";
 
 const FILES_TO_CACHE = [
   "./",
+  "./inicio.html",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
@@ -32,7 +33,7 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(resp => {
-      return resp || fetch(event.request).catch(() => caches.match("./index.html"));
+      return resp || fetch(event.request).catch(() => caches.match("./inicio.html"));
     })
   );
 });
